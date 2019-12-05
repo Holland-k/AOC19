90eca8f7768d345547c17dda745b21bcfdff4d2a
Holland <holland@hollandk.com>
2019-12-05 17:42:06 -0500

    finished day 4

--- day4/p1.hs
+++ day4/p1.hs
@@ -17,11 +17,21 @@ findAsc :: [Char] -> Bool
 findAsc (x:y:[]) = if ((digitToInt x) <= (digitToInt y))
   then True
   else False
-
 findAsc (x:y:xs) = if ((digitToInt x) <= (digitToInt y))
   then findAsc ([y] ++ xs)
   else False
 
+countC :: [Char] -> [String] -> Integer
+countC x = toInteger . length . filter (x ==)
+
+findEAsc :: [String] -> [String] -> [Integer]
+findEAsc [] _ = []
+findEAsc (x:xs) ys = [countC x ys] ++ findEAsc xs ys
+
+findE :: [String] -> [Integer]
+findE x = findEAsc x x 
+
 main = do
-  print $ length $ filter findAsc $ filter findDup $ show <$> x 
+  print $ length . filter (elem 2) . findE <$> filter findAsc $ filter findDup $ show <$> x
   where x = [138241..674034]
+      
