b2f11d7971cea2786894235c23dd94d53a98e4c1
Holland <holland@hollandk.com>
2019-12-13 18:48:41 -0500

    day 6 part 1, not working

--- day6/p1.hs
+++ day6/p1.hs
@@ -8,13 +8,15 @@ parse x = (head y, head $ tail y)
 
 findPar :: String -> -- Child
   Map String String -> -- input
-  Int
+  Int -- path count
 findPar child inp =
   case M.lookup child inp of
     Nothing -> 1
     Just tp -> 1 + findPar tp inp
 
-findPars :: Int -> Map String String -> Int
+findPars :: Int -> -- index
+  Map String String -> -- input map
+  Int -- path count
 findPars i xs = if ((length xs) > i)
   then findPar (fst (elemAt i xs)) xs + findPars (i+1) xs
   else 0
